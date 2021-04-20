0e428680ac7043b1860c73560a3f1de0322739f0
JulieSemenova <julie.loggi@gmail.com>
2021-04-20 10:04:54 +0300

    add cypress to the project

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,43 +1,25 @@
-import {userBuilder} from './generate'
-
-Cypress.Commands.add('createUser', overrides => {
-  const user = userBuilder(overrides)
-  return cy
-    .request({
-      url: 'http://localhost:3000/register',
-      method: 'POST',
-      body: user,
-    })
-    .then(({body}) => body.user)
-})
-
-Cypress.Commands.add('login', user => {
-  return cy
-    .request({
-      url: 'http://localhost:3000/login',
-      method: 'POST',
-      body: user,
-    })
-    .then(({body}) => {
-      window.localStorage.setItem('token', body.user.token)
-      return body.user
-    })
-})
-
-Cypress.Commands.add('loginAsNewUser', () => {
-  cy.createUser().then(user => {
-    cy.login(user)
-  })
-})
-
-Cypress.Commands.add('assertHome', () => {
-  cy.url().should('eq', `${Cypress.config().baseUrl}/`)
-})
-
-Cypress.Commands.add('assertLoggedInAs', user => {
-  cy.window()
-    .its('localStorage.token')
-    .should('be.a', 'string')
-    .getByTestId('username-display')
-    .should('have.text', user.username)
-})
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
