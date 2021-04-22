3e4ab36aed213216677ccd3ee6f3c190246a6fea
JulieSemenova <julie.loggi@gmail.com>
2021-04-22 10:06:47 +0300

    add test for authenticated calculator

--- cypress/e2e/calculator.js
+++ cypress/e2e/calculator.js
@@ -1,4 +1,4 @@
-describe('anonymous calculator', ()=> {
+describe('anonymous calculator', () => {
   it('can make calculations', () => {
     cy.visit('/')
     cy.findByText(/^1$/).click()
@@ -6,5 +6,21 @@ describe('anonymous calculator', ()=> {
     cy.findByText(/^2$/).click()
     cy.findByText(/^=$/).click()
     cy.findByTestId('result').should('have.text', '3')
+  })
+
+})
+describe('authenticated calculator', () => {
+
+  it('displays the username', () => {
+    cy.createUser().then(user => {
+      cy.visit('/')
+      cy.findByText(/login/i).click()
+      cy.findByLabelText(/username/i).type(user.username)
+      cy.findByLabelText(/password/i).type(user.password)
+      cy.findByText(/submit/i).click()
+      cy.findByTestId('username-display').should('have.text', user.username)
+      cy.findByText(/logout/i).click()
+      cy.findByTestId('username-display').should('not.exist')
+    })
   })
 })
