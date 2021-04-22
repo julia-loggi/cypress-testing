4b2381bd8414ad77e5d2b1d72c38cd7efd618948
JulieSemenova <julie.loggi@gmail.com>
2021-04-22 10:13:11 +0300

    add custom cypress command for login user

--- cypress/e2e/calculator.js
+++ cypress/e2e/calculator.js
@@ -13,13 +13,7 @@ describe('authenticated calculator', () => {
 
   it('displays the username', () => {
     cy.createUser().then(user => {
-      cy.request({
-        url: 'http://localhost:3000/login',
-        method: 'POST',
-        body: user,
-      }).then(response => {
-        window.localStorage.setItem('token', response.body.user.token)
-      })
+      cy.login(user)
       cy.visit('/')
       cy.findByTestId('username-display').should('have.text', user.username)
       cy.findByText(/logout/i).click()
