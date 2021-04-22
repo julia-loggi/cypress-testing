65d1c38a03a2a807fc2ed01cb6b200affe16ae20
JulieSemenova <julie.loggi@gmail.com>
2021-04-22 09:21:26 +0300

    test for error on error registration

--- cypress/e2e/register.js
+++ cypress/e2e/register.js
@@ -13,4 +13,16 @@ describe('registration', () => {
     cy.window().its('localStorage.token').should('be.a', 'string')
     cy.findByTestId('username-display').should('have.text', user.username)
   })
+
+  it('it should show error message if there is an error', () => {
+    cy.server().route({
+      method: 'POST',
+      url: 'http://localhost:3000/register',
+      status: 500,
+      response: {},
+    })
+    cy.visit('/register')
+    cy.findByText(/submit/i).click()
+    cy.findByText(/error.*try again/i)
+  })
 })
